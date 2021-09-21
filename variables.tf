17bc1a5c7079cd117b0ddfd03e41a5a6a3b83878
Andr1500 <64834299+Andr1500@users.noreply.github.com>
2021-09-21 18:40:28 +0200

    add variable Region

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "gcp_project" {
+  description = "The project where you want to set up environment"
+  default     = "notejam-325412"
+}
+
 variable "gcp_region" {
   description = "The region where you want to run Cloud Run Service"
   type        = string
